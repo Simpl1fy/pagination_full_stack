b91ba1eb085b78a3bfd0ca604530d45d52d99ae6
Simpl1fy <gourabdas07012004@gmail.com>
2025-03-06 19:10:10 +0530

    created a get route for paginated data, using query parameters to send limited data based on page number

--- pagination-server/src/server.ts
+++ pagination-server/src/server.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 import connectDB from './database/db';
 import Data from './database/Model/dataSchema';
+import { rmSync } from 'fs';
 
 const app = express();
 
@@ -35,6 +36,29 @@ app.post('/add', async (req: Request, res: Response) => {
     })
   } catch(err) {
     console.log("An error occured while inserting data =", (err as Error).message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+})
+
+app.get('/data', async(req: Request, res: Response) => {
+  try {
+    const { page, limit } = req.query;
+    const pageNumber = Number(page) || 1;
+    const limitNumber = Number(limit) || 5;
+
+    const skipData = (pageNumber - 1) * limitNumber;
+
+    const resData = await Data.find().skip(skipData).limit(limitNumber);
+    // console.log(resData);
+
+    const cRes = await Data.find();
+    const totalDocs = cRes.length;
+    res.status(200).json({
+      data: resData,
+      nbHits: totalDocs
+    });
+  } catch(err) {
+    console.log("An error occured while fetching paginated data =", (err as Error).message);
     res.status(500).json({message: "Internal Server Error"});
   }
 })
