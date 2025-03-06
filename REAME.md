79326016cce307856acd33a3e259f81b109a60e1
Simpl1fy <gourabdas07012004@gmail.com>
2025-03-06 17:04:44 +0530

    setup frontend and backend with typescript

---
+++ REAME.md
@@ -0,0 +1,1 @@
+This repo is for learning pagination in both backend and frontend
---
+++ pagination-react/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl">Server Pagination</h1>
+    </>
+  )
+}
+
+export default App
